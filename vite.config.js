b598a23fc0114c4836b6c72d0560d8e459d301e7
Vladimir Grigorov <punktc77@gmail.com>
2025-10-27 16:24:39 -0300

    add to gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Portfolio/',
+      // Установлено относительное базовое значение для корректной работы на GitHub Pages
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
