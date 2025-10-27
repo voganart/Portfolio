9c3fbc45af9b23784c924035cb74e74b8c1908fa
Vladimir Grigorov <punktc77@gmail.com>
2025-10-27 17:03:32 -0300

    Update base path in vite.config.js to ensure correct asset loading for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './', // либо '/Portfolio/' если хочешь абсолютный путь в ветке gh-pages
+    base: '/Portfolio/', // либо '/Portfolio/' если хочешь абсолютный путь в ветке gh-pages
     plugins: [react()],
     resolve: {
       alias: {
