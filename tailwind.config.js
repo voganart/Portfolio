34f0073358cdc4e94de74042487e0255aa1a246e
Vladimir Grigorov <punktc77@gmail.com>
2026-02-26 00:05:56 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,16 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Указываем, где используются классы Tailwind
   content: [
     "./index.html",
     "./App.tsx",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
-
-  // В этой секции мы подключаем стандартную тему и расширяем её
   theme: {
-    ...defaultTheme, // <-- Вот эта строка подключает все стандартные стили (цвета, отступы и т.д.)
     extend: {
-      // А здесь мы добавляем свои кастомные стили или переопределяем стандартные
+      // Здесь мы добавляем свои кастомные стили или переопределяем стандартные
       fontFamily: {
-        // Добавляем Rubik в стек шрифтов без засечек, сохраняя стандартные
-        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+        // Добавляем Rubik в стек шрифтов без засечек
+        sans: ['Rubik', 'sans-serif'],
       },
     },
   },
